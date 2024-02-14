33f7a50d29ca3c854d53cad93fa27ef670fa7afa
Jacob Golub <jacobsolomongolub@gmail.com>
2024-02-14 17:28:59 +0100

    Fixed setState bug, added actions and step function

--- factory_q_learning.py
+++ factory_q_learning.py
@@ -19,6 +19,10 @@ The state will be a list: [agentPosition, agentLoad, stationLoads]
 Consequently, the state-space will be a list of these dictionaries.
 How many possible states? : no.free_spaces*(agent_max+1)*Product(station_max+1)
 
+For the load and unload actions, I will enforce that when the agent is at a station, it is not adjacent to any
+other station. This means that when we perform this action, it only has one station to consider.
+This assumption can be enforced by further discretising the factory floor if needed.
+
 '''
 
 import numpy as np
@@ -91,8 +95,8 @@ class Factory:
 
         self.createStateSpace(agent)
         print(self.grid)
-        #self.actionSpace = {"Up":self.move(0), }
-        self.possibleActions = ["Up", "Down", "Left", "Right", "Load", "Unload"]
+        print("Initial state: ", self.getState())
+        self.possibleActions = ["Up", "Right", "Down", "Left", "Load", "Unload"]
     
 
     def addAgent(self):
@@ -155,11 +159,104 @@ class Factory:
         self.agent.load = state[1]
         i = 0
         for station in self.stations:
-            station.load = state[i+1]
+            station.load = state[i+2]
             i+=1
         
         #Update the grid:
         self.addAgent()
+
+    def action(self,action):
+        if action == "Up":
+            return self.move(0)
+        elif action == "Right":
+            return self.move(1)
+        elif action == "Down":
+            return self.move(2)
+        elif action == "Left":
+            return self.move(3)
+        elif action == "Load":
+            return self.load()
+        elif action == "Unload":
+            return self.unload()
+
+    def move(self,key):
+        currentX, currentY = self.agent.position
+        if key == 0:
+            newX = currentX - 1
+            return [(newX,currentY),self.agent.load] + [stationLoad.load for stationLoad in self.stations]
+
+        elif key == 1:
+            newY = currentY + 1
+            return [(currentX,newY),self.agent.load] + [stationLoad.load for stationLoad in self.stations]
+        
+        elif key == 2:
+            newX = currentX + 1
+            return [(newX,currentY),self.agent.load] + [stationLoad.load for stationLoad in self.stations]
+        
+        elif key == 3:
+            newY = currentY - 1
+            return [(currentX,newY),self.agent.load] + [stationLoad.load for stationLoad in self.stations]            
+
+    def load(self):
+        currentAgentPosition = self.agent.position
+        currentAgentLoad = self.agent.load
+        stationPositions = [station.position for station in self.stations]
+        currentStationLoads = [station.load for station in self.stations]
+
+        # Now figure out which station is the agent next to
+        i = 0
+        for possibleStationPosition in stationPositions:
+            if manhattenDistance(currentAgentPosition, possibleStationPosition) == 1:
+                newAgentLoad = currentAgentLoad + 1
+                newStationLoad = currentStationLoads[i] - 1
+                newStationLoads = currentStationLoads[:i] + [newStationLoad] + currentStationLoads[i+1:]
+                return [currentAgentPosition, newAgentLoad] + newStationLoads
+            i+=1
+        
+        return [currentAgentPosition, currentAgentLoad] + currentStationLoads
+                
+
+    def unload(self):
+        currentAgentPosition = self.agent.position
+        currentAgentLoad = self.agent.load
+        stationPositions = [station.position for station in self.stations]
+        currentStationLoads = [station.load for station in self.stations]
+
+        if manhattenDistance(currentAgentPosition,self.deposit) == 1:
+            return [currentAgentPosition, currentAgentLoad-1] + currentStationLoads
+
+        # Now figure out which station is the agent next to
+        i = 0
+        for possibleStationPosition in stationPositions:
+            if manhattenDistance(currentAgentPosition, possibleStationPosition) == 1:
+                newAgentLoad = currentAgentLoad - 1
+                newStationLoad = currentStationLoads[i] + 1
+                newStationLoads = currentStationLoads[:i] + [newStationLoad] + currentStationLoads[i+1:]
+                return [currentAgentPosition, newAgentLoad] + newStationLoads
+            i+=1
+        
+        return [currentAgentPosition, currentAgentLoad] + currentStationLoads        
+
+
+    def illegalState(self,newState):
+
+        if newState not in self.stateSpacePlus:
+            return True
+        else:
+            return False
+
+    def step(self, action):
+        trialState = self.action(action)
+        if not self.illegalState(trialState):
+            self.setState(trialState)
+        newState = self.getState()
+
+        if self.isTerminalState(newState):
+            reward = 0
+        else:
+            reward = -1
+
+        return newState, reward, self.isTerminalState(newState) 
 
 # This function helps with generating the variations of station loads (made by ChatGPT)
 def generate_variations(lst):
@@ -181,7 +278,10 @@ def generate_variations(lst):
     generate_helper(lst)
     return [list(variation) for variation in variations]
 
-
+def manhattenDistance(pos1,pos2):
+    x1, y1 = pos1
+    x2, y2 = pos2
+    return abs(x2-x1) + abs(y2-y1)
 
 myObstacles = [(2,1)]
 station1 = Station(2,3,(0,1))
@@ -190,3 +290,9 @@ myStations = [station1,station2]
 myAgent = Agent(0,2,(3,0))
 deposit = (3,2)
 myFactory = Factory(3,4,myObstacles,myStations,myAgent,deposit)
+myFactory.step("Up")
+myFactory.step("Up")
+myFactory.step("Right")
+myFactory.step("Load")
+print(myFactory.getState())
+print(myFactory.grid)

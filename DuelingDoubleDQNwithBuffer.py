971dc326be2e59086d7b75157ea52effd6bfa403
Jacob Golub <jacobsolomongolub@gmail.com>
2024-03-18 16:37:06 +0100

    Implemented prioritised replay without bias annealing

--- DuelingDoubleDQNwithBuffer.py
+++ DuelingDoubleDQNwithBuffer.py
@@ -61,21 +61,23 @@ class Agent:
                                    input_dims=input_dims,
                                    fc1_dims=8, fc2_dims=16)
 
-        self.state_memory = np.zeros((self.mem_size, input_dims),
-                                     dtype=np.float32)
-        self.new_state_memory = np.zeros((self.mem_size, input_dims),
-                                         dtype=np.float32)
-        self.action_memory = np.zeros(self.mem_size, dtype=np.int32)
-        self.reward_memory = np.zeros(self.mem_size, dtype=np.float32)
-        self.terminal_memory = np.zeros(self.mem_size, dtype=bool)
+        #self.state_memory = np.zeros((self.mem_size, input_dims),
+        #                             dtype=np.float32)
+        #self.new_state_memory = np.zeros((self.mem_size, input_dims),
+        #                                 dtype=np.float32)
+        #self.action_memory = np.zeros(self.mem_size, dtype=np.int32)
+        #self.reward_memory = np.zeros(self.mem_size, dtype=np.float32)
+        #self.terminal_memory = np.zeros(self.mem_size, dtype=bool)
+        self.memory = help.PrioritizedReplayBuffer(size=self.mem_size,alpha=0.6)
 
     def store_transition(self, state, action, reward, state_, terminal):
-        index = self.mem_cntr % self.mem_size
-        self.state_memory[index] = state
-        self.new_state_memory[index] = state_
-        self.reward_memory[index] = reward
-        self.action_memory[index] = action
-        self.terminal_memory[index] = terminal
+        #index = self.mem_cntr % self.mem_size
+        #self.state_memory[index] = state
+        #self.new_state_memory[index] = state_
+        #self.reward_memory[index] = reward
+        #self.action_memory[index] = action
+        #self.terminal_memory[index] = terminal
+        self.memory.add(state,action,reward,state_,terminal)
 
         self.mem_cntr += 1
 
@@ -116,19 +118,27 @@ class Agent:
 
         self.replace_target_network()
 
-        max_mem = min(self.mem_cntr, self.mem_size)
+        #state_batch = T.tensor(self.state_memory[batch]).to(self.Q_eval.device)
+        #new_state_batch = T.tensor(
+        #        self.new_state_memory[batch]).to(self.Q_eval.device)
+        #action_batch = self.action_memory[batch]
+        #reward_batch = T.tensor(
+        #        self.reward_memory[batch]).to(self.Q_eval.device)
+        #terminal_batch = T.tensor(
+        #        self.terminal_memory[batch]).to(self.Q_eval.device)
+        
+        samples = self.memory.sample(self.batch_size,beta = 0.4)
+        state_batch = T.tensor(samples[0]).to(self.Q_eval.device)
+        new_state_batch = T.tensor(samples[3]).to(self.Q_eval.device)
+        action_batch = samples[1]
+        reward_batch = T.tensor(samples[2]).to(self.Q_eval.device)
+        terminal_batch = T.tensor(samples[4]).to(self.Q_eval.device)
+        weights = samples[5]
+        batch_idxes = samples[6]
 
-        batch = np.random.choice(max_mem, self.batch_size, replace=False)
-        batch_index = np.arange(self.batch_size, dtype=np.int32)
 
-        state_batch = T.tensor(self.state_memory[batch]).to(self.Q_eval.device)
-        new_state_batch = T.tensor(
-                self.new_state_memory[batch]).to(self.Q_eval.device)
-        action_batch = self.action_memory[batch]
-        reward_batch = T.tensor(
-                self.reward_memory[batch]).to(self.Q_eval.device)
-        terminal_batch = T.tensor(
-                self.terminal_memory[batch]).to(self.Q_eval.device)
+        weights = np.sqrt(weights)
+        weights = T.FloatTensor(weights).to(self.Q_eval.device)
 
         #q = self.Q_eval.forward(state_batch)[batch_index, action_batch]
         q = T.zeros(self.batch_size)
@@ -165,11 +175,14 @@ class Agent:
         #print("Rewards:", reward_batch)
 
         q[T.logical_not(terminal_batch)] = reward_batch[T.logical_not(terminal_batch)] + self.gamma*q_target[T.logical_not(terminal_batch)].squeeze()
-        q[terminal_batch] = reward_batch[terminal_batch]
+        q[terminal_batch] = reward_batch[terminal_batch].float()
         #print("q_values: ", q)
         #q_target[maxA] = reward_batch + self.gamma*T.max(q_next, dim=1)
-        #print(q.size(), q_eval.size())
-        loss = self.Q_eval.loss(q, q_eval.squeeze()).to(self.Q_eval.device)
+        TD_error = q - q_eval.squeeze()
+        weighted_TD_errors = T.mul(TD_error, weights)
+        zero_tensor = T.zeros(weighted_TD_errors.shape)
+        
+        loss = self.Q_eval.loss(weighted_TD_errors, zero_tensor).to(self.Q_eval.device)
         loss.backward()
         self.Q_eval.optimizer.step()
 
@@ -178,6 +191,10 @@ class Agent:
         if self.iter_cntr % 1000 == 0:
             self.epsilon = self.epsilon*0.99
             print("Epsilon: ",self.epsilon)
+
+        td_errors = TD_error.detach().numpy()
+        new_priorities = np.abs(td_errors) + 1e-6
+        self.memory.update_priorities(batch_idxes, new_priorities)
         #self.epsilon = self.epsilon - self.eps_dec \
         #    if self.epsilon > self.eps_min else self.eps_min
 
